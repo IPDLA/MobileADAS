ca8b8392a6822d6fc430615bc4b165c73a8c431e
Jaehun-Jo <chojh922@gmail.com>
2022-07-17 17:13:03 +0900

    #9 [feature] 실시간 속도 기능 구현 & GPS 권한 요청

--- app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
+++ app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
@@ -2,14 +2,14 @@ package com.ipdla.mobileadas.ui.main
 
 import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
+import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.core.content.ContextCompat
 import com.ipdla.mobileadas.R
 import com.ipdla.mobileadas.databinding.ActivityMainBinding
 import com.ipdla.mobileadas.ui.base.BaseActivity
@@ -21,39 +21,59 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
     private lateinit var locationManager: LocationManager
     private lateinit var locationListener: LocationListener
-
+    private lateinit var lastLocation: Location
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.mainViewModel = mainViewModel
 
+        initGPSPermission()
         initActivityResultLauncher()
         initSpeedCheck()
         initGuideBtnClickListener()
         initSoundBtnClickListener()
     }
 
+    private fun initGPSPermission() {
+        val locationPermissionRequest = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            when {
+                permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                    // Precise location access granted.
+                }
+                permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                    // Only approximate location access granted.
+                }
+                else -> {
+                    // No location access granted.
+                }
+            }
+        }
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+    }
+
     private fun initSpeedCheck() {
         locationManager = this.getSystemService(LOCATION_SERVICE) as LocationManager
         locationListener = LocationListener { location ->
+            Log.d("testtt", location.toString())
+            var deltaTime = 0.0
+            var speed = 0.0
             if (location.hasSpeed()) {
-                mainViewModel.initSpeed(location.speed.toInt())
+
+                if (this::lastLocation.isInitialized) {
+                    deltaTime = (location.time - lastLocation.time) / 1000.0
+                    speed = lastLocation.distanceTo(location) / deltaTime
+                }
+                lastLocation = location
+                Log.d("testtt", speed.toString())
+                mainViewModel.initSpeed(speed.toInt())
             }
         }
 
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                1000L,
-                1f,
-                locationListener)
-        }
+
     }
 
     private fun initActivityResultLauncher() {
