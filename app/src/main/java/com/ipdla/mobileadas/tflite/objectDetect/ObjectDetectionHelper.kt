ac5ab7908c3a81217c8b061d8c7f700b2514a83b
KangSungBum <peter9706@naver.com>
2022-11-02 17:51:24 +0900

    problem: camera and preview

--- app/src/main/java/com/ipdla/mobileadas/tflite/objectDetect/ObjectDetectionHelper.kt
+++ app/src/main/java/com/ipdla/mobileadas/tflite/objectDetect/ObjectDetectionHelper.kt
@@ -1,48 +1,89 @@
 package com.ipdla.mobileadas.tflite.objectDetect
 
-import android.graphics.RectF
-import org.tensorflow.lite.Interpreter
+import android.content.Context
+import android.graphics.Bitmap
+import android.os.SystemClock
+import android.util.Log
+import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.Rot90Op
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.vision.detector.Detection
+import org.tensorflow.lite.task.vision.detector.ObjectDetector
 
+class ObjectDetectionHelper(
+    var threshold: Float = 0.5f,
+    var numThreads: Int = 2,
+    var maxResults: Int = 3,
+    var currentDelegate: Int = 0,
+    var currentModel: Int = 0,
+    val context: Context,
+    val objectDetectorListener: DetectorListener?
+) {
+    private var objectDetector: ObjectDetector? = null
 
-class ObjectDetectionHelper(private val tflite: Interpreter, private val labels: List<String>) {
-    data class ObjectPrediction(val location: RectF, val label: String, val score: Float)
+    fun clearObjectDetector() {
+        objectDetector = null
+    }
 
-    private val locations = arrayOf(Array(OBJECT_COUNT) { FloatArray(4) })
-    private val labelIndices =  arrayOf(FloatArray(OBJECT_COUNT))
-    private val scores =  arrayOf(FloatArray(OBJECT_COUNT))
+    fun setObjectDetector(){
+        val optionsBuilder =
+            ObjectDetector.ObjectDetectorOptions.builder()
+                .setScoreThreshold(0.7f)
+                .setMaxResults(3)
 
-    private val outputBuffer = mapOf(
-        0 to locations,
-        1 to labelIndices,
-        2 to scores,
-        3 to FloatArray(1)
-    )
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(2)
+        optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
 
-    val predictions get() = (0 until OBJECT_COUNT).map {
-        ObjectPrediction(
+        val modelName = "mobilenetv1.tflite"
 
-            //[상,좌,하,우]output
-            location = locations[0][it].let {
-                RectF(it[1], it[0], it[3], it[2])
-            },
+        try {
+            objectDetector =
+                ObjectDetector.createFromFileAndOptions(context, modelName, optionsBuilder.build())
+        } catch (e: IllegalStateException) {
+            objectDetectorListener?.onError(
+                "Object detector failed to initialize. See error logs for details"
+            )
+            Log.e("Test", "TFLite failed to load model with error: " + e.message)
+        }
+    }
 
-            // SSD Mobilenet V1 Model assumes class 0 is background class
-            // in label file and class labels start from 1 to number_of_classes + 1,
-            // while outputClasses correspond to class index from 0 to number_of_classes
-            label = labels[1 + labelIndices[0][it].toInt()],
+    fun detect(image: Bitmap, imageRotation: Int) {
+        if (objectDetector == null) {
+            setObjectDetector()
+        }
 
-            // Score is a single value of [0, 1]
-            score = scores[0][it]
+        // Inference time is the difference between the system time at the start and finish of the
+        // process
+        var inferenceTime = SystemClock.uptimeMillis()
+
+        // Create preprocessor for the image.
+        // See https://www.tensorflow.org/lite/inference_with_metadata/
+        //            lite_support#imageprocessor_architecture
+        val imageProcessor =
+            ImageProcessor.Builder()
+                .add(Rot90Op(-imageRotation / 90))
+                .build()
+
+        // Preprocess the image and convert it into a TensorImage for detection.
+        val tensorImage = imageProcessor.process(TensorImage.fromBitmap(image))
+
+        val results = objectDetector?.detect(tensorImage)
+        inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+        objectDetectorListener?.onResults(
+            results,
+            inferenceTime,
+            tensorImage.height,
+            tensorImage.width)
+    }
+
+    interface DetectorListener {
+        fun onError(error: String)
+        fun onResults(
+            results: MutableList<Detection>?,
+            inferenceTime: Long,
+            imageHeight: Int,
+            imageWidth: Int
         )
-    }
-
-    fun predict(image: TensorImage): List<ObjectPrediction> {
-        tflite.runForMultipleInputsOutputs(arrayOf(image.buffer), outputBuffer)
-        return predictions
-    }
-
-    companion object {
-        const val OBJECT_COUNT = 10
     }
 }
--- app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
+++ app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
@@ -4,19 +4,28 @@ import android.Manifest
 import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.content.res.Configuration
+import android.graphics.Bitmap
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.*
 import com.ipdla.mobileadas.BuildConfig.TMAP_API_KEY
 import com.ipdla.mobileadas.R
 import com.ipdla.mobileadas.databinding.ActivityMainBinding
+import com.ipdla.mobileadas.tflite.objectDetect.ObjectDetectionHelper
 import com.ipdla.mobileadas.ui.base.BaseActivity
 import com.ipdla.mobileadas.ui.destination.DestinationActivity
 import com.ipdla.mobileadas.ui.main.viewmodel.MainViewModel
@@ -27,10 +36,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import org.tensorflow.lite.task.vision.detector.Detection
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import kotlin.math.abs
 import kotlin.system.exitProcess
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), ObjectDetectionHelper.DetectorListener {
     private val mainViewModel by viewModels<MainViewModel>()
     private lateinit var animationColorChange: Animator
     private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
@@ -41,6 +54,14 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     private lateinit var previousLocation: Location
     private lateinit var presentTMapPoint: TMapPoint
     private lateinit var destinationPoint: TMapPoint
+
+    private lateinit var cameraExecutor: ExecutorService
+    private lateinit var objectDetectorHelper: ObjectDetectionHelper
+    private lateinit var bitmapBuffer: Bitmap
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,6 +75,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         initSoundBtnClickListener()
         initCautionAnimator()
         initIsCautionObserver()
+
+        initDectector()
     }
 
     private fun initCautionAnimator() {
@@ -232,9 +255,156 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onDestroy() {
         super.onDestroy()
         stopLocationUpdates()
+        cameraExecutor.shutdown()
     }
 
     companion object {
         const val REQUEST_LOCATION_PERMISSION = 0
+        const val REQUEST_CAMERA_PERMISSION = 1
+    }
+
+
+    private fun initDectector() {
+        Log.d("abcd","init Detector")
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA),
+                REQUEST_CAMERA_PERMISSION
+            )
+        }
+
+        objectDetectorHelper = ObjectDetectionHelper(
+            context = this,
+            objectDetectorListener = this)
+
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        binding.mainPreviewView.post{
+            setUpCamera()
+        }
+    }
+
+
+
+    private fun setUpCamera() {
+        Log.d("abcd","init setUpCamera")
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(applicationContext)
+        cameraProviderFuture.addListener(
+            {
+                // CameraProvider
+                cameraProvider = cameraProviderFuture.get()
+
+                // Build and bind the camera use cases
+                bindCameraUseCases()
+            },
+            ContextCompat.getMainExecutor(this)
+        )
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindCameraUseCases() {
+        Log.d("abcd","init bindCameraUse")
+        // CameraProvider
+        val cameraProvider =
+            cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        // CameraSelector - makes assumption that we're only using the back camera
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview =
+            Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(binding.mainPreviewView.display.rotation)
+                .build()
+        Log.d("abcd","preview Passed")
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(binding.mainPreviewView.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    Log.d("abcd","imageAnalyzer initiated")
+                    it.setAnalyzer(cameraExecutor) { image ->
+                        Log.d("abcd","image analyzer failed")
+                        if (!::bitmapBuffer.isInitialized) {
+                            // The image rotation and RGB image buffer are initialized only once
+                            // the analyzer has started running
+                            Log.d("abcd","imageAnalyzer Entered")
+                            bitmapBuffer = Bitmap.createBitmap(
+                                image.width,
+                                image.height,
+                                Bitmap.Config.ARGB_8888
+                            )
+                        }
+                        Log.d("abcd","after bitmapBuffer")
+
+                        detectObjects(image)
+                    }
+                }
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer)
+
+            // Attach the viewfinder's surface provider to preview use case
+//            preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e("TAG", "Use case binding failed", exc)
+        }
+    }
+
+    private fun detectObjects(image: ImageProxy) {
+        Log.d("abcd","init detectObjects")
+        // Copy out RGB bits to the shared bitmap buffer
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+        val imageRotation = image.imageInfo.rotationDegrees
+        // Pass Bitmap and rotation to the object detector helper for processing and detection
+        objectDetectorHelper.detect(bitmapBuffer, imageRotation)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        imageAnalyzer?.targetRotation = binding.mainPreviewView.display.rotation
+    }
+
+    override fun onError(error: String) {
+//        activity?.runOnUiThread {
+//            Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+//        }
+    }
+
+    override fun onResults(
+        results: MutableList<Detection>?,
+        inferenceTime: Long,
+        imageHeight: Int,
+        imageWidth: Int
+    ) {
+        Log.d("abcd", "${results?.get(0)!!.categories[0].label}")
+//        activity?.runOnUiThread {
+//            // Pass necessary information to OverlayView for drawing on the canvas
+//            fragmentCameraBinding.overlay.setResults(
+//                results ?: LinkedList<Detection>(),
+//                imageHeight,
+//                imageWidth
+//            )
+//
+//            // Force a redraw
+//            fragmentCameraBinding.overlay.invalidate()
+//        }
     }
 }
