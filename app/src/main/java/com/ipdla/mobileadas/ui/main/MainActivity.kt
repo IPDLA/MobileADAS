4ae765e3fb7e40eeed6bd52f9747680034e62e3e
Jaehun-Jo <chojh922@gmail.com>
2022-11-11 17:12:52 +0900

    #77 [Fix] 오타 수정

--- app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
+++ app/src/main/java/com/ipdla/mobileadas/ui/main/MainActivity.kt
@@ -74,8 +74,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                     var speed = lastLocation.speed.toDouble()
                     speed *= METER_PER_SEC_TO_KILOMETER_PER_HOUR * SPEED_CORRECTION_VALUE
                     mainViewModel.initSpeed(speed.toInt())
+                    lastTMapPoint = TMapPoint(lastLocation.latitude, lastLocation.longitude)
                     if (mainViewModel.isGuide.value == true) UpdateTMapView()
-                    lastTMapPoint = TMapPoint(lastTMapPoint.latitude, lastTMapPoint.longitude)
                 }
             }
         }
