6d4503fdc4bc52eb77d4bf15436f03053ad0c762
JJH <jakebbak@gmail.com>
2022-12-06 21:22:45 +0900

    #13 어린이 보호 구역 지시 표지판 정확도 개선 완료

--- app/src/main/java/com/ipdla/mobileadas/tflite/objectDetect/ObjectDetectionHelper.kt
+++ app/src/main/java/com/ipdla/mobileadas/tflite/objectDetect/ObjectDetectionHelper.kt
@@ -50,7 +50,7 @@ class ObjectDetectionHelper(
     fun setTrafficDetector(){
         val optionsBuilder =
             ObjectDetector.ObjectDetectorOptions.builder()
-                .setScoreThreshold(0.6f)
+                .setScoreThreshold(0.8f)
                 .setMaxResults(3)
         val baseOptionsBuilder = BaseOptions.builder().setNumThreads(2)
         optionsBuilder.setBaseOptions(baseOptionsBuilder.build())
